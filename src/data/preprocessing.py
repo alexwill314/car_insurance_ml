84100302e7fc2e6c1e2c8f5ba559c6bcc5a99a17
alexwill314 <will_alexander@outlook.de>
2026-06-06 17:50:08 +0200

    added preprocessing for modeling

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -20,3 +20,6 @@ def preprocess_raw_data(freq_df: pd.DataFrame, sev_df: pd.DataFrame):
 
 def preprocess_data_model(df:pd.DataFrame):
     df = df.copy()
+    df["ClaimNb"] = df["ClaimNb"].clip(upper=4)
+    df["Exposure"] = df["Exposure"].clip(upper=1.1)
+    df.loc[(df["ClaimNb"] > 0) & (df["ClaimAmount"] == 0), "ClaimNb"] = 0
